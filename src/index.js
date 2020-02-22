913fc55e76c0707866af0fb4d4d38c1f8bc082a9
Michael-Lock <michael.lock32@gmail.com>
2020-02-22 12:25:15 +1030

    Cleaned default files. Migrated in existing TimerBlock code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import TimerBlock from './TimerBlock';
 import * as serviceWorker from './serviceWorker';
 
-var element = React.createElement('h1', {className: 'greeting'}, "Hello World!")
-
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(
+    <TimerBlock label="Total Time" baseSeconds={5500} currentSeconds={0} isCounting={true}/>,
+    document.getElementById("root")
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
