7b9774177082ae9e7e6e6e25aaa0f888fb2ef360
Michael-Lock <michael.lock32@gmail.com>
2020-02-24 19:46:52 +1030

    Updates

---
+++ precache-manifest.43888b1cf435b523febbaaa5e42a56e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd6edb1c70d49bbc029a705bfe3515e",
+    "url": "/ti4companion/index.html"
+  },
+  {
+    "revision": "dac8cbcc80d0637c603e",
+    "url": "/ti4companion/static/css/main.37f24418.chunk.css"
+  },
+  {
+    "revision": "15b86adaf63380c01172",
+    "url": "/ti4companion/static/js/2.4c21f63d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ti4companion/static/js/2.4c21f63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac8cbcc80d0637c603e",
+    "url": "/ti4companion/static/js/main.ff1b384c.chunk.js"
+  },
+  {
+    "revision": "3b6c9246c9cebda97847",
+    "url": "/ti4companion/static/js/runtime-main.0a554107.js"
+  },
+  {
+    "revision": "4fffe16bd65fe6bb35f19b01cb8603d8",
+    "url": "/ti4companion/static/media/multiple-users-silhouette-grey.4fffe16b.svg"
+  },
+  {
+    "revision": "2aa64ca8fad9a9fd40fabaaf0e412b97",
+    "url": "/ti4companion/static/media/multiple-users-silhouette.2aa64ca8.svg"
+  }
+]);
